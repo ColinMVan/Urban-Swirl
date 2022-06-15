a84eeb5ab1b2a837186f5270bc16e77a09423d0e
ColinMVan <colin_vanhoveln@outlook.com>
2022-06-15 19:41:41 -0400

    Changed the brightness of the backgrounds and linked the images via the internet instead of by local

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const lightButton = document.querySelector("#light");
 const darkButton = document.querySelector("#dark");
 
 lightButton.onclick = () => {
-  document.documentElement.style.setProperty("--background", "hsl(0, 0%, 95%)");
+  document.documentElement.style.setProperty("--background", "hsl(0, 0%, 90%)");
 };
 
 darkButton.onclick = () => {
-  document.documentElement.style.setProperty("--background", "hsl(0, 0%, 3%)");
+  document.documentElement.style.setProperty("--background", "hsl(0, 0%, 8%)");
 };
